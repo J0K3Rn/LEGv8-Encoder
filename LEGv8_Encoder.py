857ebc71f83589c9649f42c341c4aafa6702deba
J0K3Rn <JosephKeeganRichardson@gmail.com>
2021-06-19 12:58:14 -0500

    Adding main test file

---
+++ LEGv8_Encoder.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# todo: add instruction functions
+
+def main():
+    input_file = input("Input file name: ")
+    output_file = input("Output file name: ")
+    print("The input file is: \'" + input_file + "\' and the output file is: \'" + output_file + "\'.")
+    # Open file. Add try catch if file doesn't exist
+    #f = open( ,"r")
+
+
+
+if __name__ == "__main__":
+    main()
