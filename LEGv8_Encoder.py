8a07dcd39a9cd9ebdb7cb94386d6bffb08f3e285
J0K3R <j0k3r@Haelion.attlocal.net>
2021-06-21 12:20:35 -0500

    Adding input file and updating code

--- LEGv8_Encoder.py
+++ LEGv8_Encoder.py
@@ -7,9 +7,20 @@ def main():
     output_file = input("Output file name: ")
     print("The input file is: \'" + input_file + "\' and the output file is: \'" + output_file + "\'.")
     # Open file. Add try catch if file doesn't exist
-    #f = open( ,"r")
+    #inf = open( ,"r")
+    outf = open( ,"w")
 
+    lines = inf.readlines()
 
+    count = 0
+    for line in lines:
+        count += 1
+        tmp = line.replace(',', '') #Remove all comma's to make splitting easier
+        # Check if there are []'s
+        tmp = line.split(' ') # Separate commands by space
+
+        
+        outf.write(line + " " + binaryCode +  " " + hexcode)
 
 if __name__ == "__main__":
     main()
