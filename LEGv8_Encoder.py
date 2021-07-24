f5150507977f687a9ca86e32923b640898c385d4
J0K3Rn <JosephKeeganRichardson@gmail.com>
2021-07-23 23:55:19 -0500

    Updating README

--- LEGv8_Encoder.py
+++ LEGv8_Encoder.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
     A simple python script that converts LEGv8 Instruction code into Machine Code. Done for ELET3405
-    Program will prompt user for an input file and output file. The input file is presumed to have no errors.
+    Program will prompt user for an input file and output file separated by a space. The input file is presumed to have no errors.
     The input file will contain LEGv8 Assembly code instructions.
-    THe output file will contain the LEGv8 Assembly code instruction, it's binary representation and its hexidecimal value in that respective order.
+    The output file will contain the LEGv8 Assembly code instruction, it's binary representation and its hexidecimal value in that respective order.
     For Example:
         in.txt:
             ADD X9, X20, X21
@@ -12,7 +12,7 @@
             ADD X9, X20, X21 10001011000101010000001010001001 0x8b150289
             LDUR X2, [X20, #8] 11111000010000001000001010000010 0xf8408282
     
-    To run: simply execute the program with ./LEGv8_Encoder.py
+    To run: Simply execute the program with ./LEGv8_Encoder.py
     Please provide the file extension when entering the input and output file.
 """
 
